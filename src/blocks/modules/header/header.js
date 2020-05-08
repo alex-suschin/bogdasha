448980252557b9b366da1ff4f158e9ab535772e1
alex-suschin <alex_suschin@mail.ru>
2020-05-08 17:03:01 +0300

    home-mob-full

---
+++ src/blocks/modules/header/header.js
@@ -0,0 +1,49 @@
+$(function() {
+
+    $('#hamburger-icon').click(function(i) {
+        $(this).addClass('active');
+
+        var menuMob = $('top-menu');
+
+
+        if (menuMob.hasClass('active') == false) {
+            $('#hamburger-icon').addClass('active');
+            $('.top-menu').addClass('active');
+            $('html').addClass('ov-h');
+
+
+            var firstClickMenu = true;
+            $(document).bind('click.myEventMenu', function(i) {
+                if (!firstClickMenu && $(i.target).closest('.top-menu').length == 0) {
+                    $('#hamburger-icon').removeClass('active');
+                    $('.top-menu').removeClass('active');
+                    $('html').removeClass('ov-h');
+                    $(document).unbind('click.myEventMenu');
+                }
+
+                firstClickMenu = false;
+            });
+        }
+
+        i.preventDefault();
+    });
+
+    $(window).on('load resize', function() {
+        var width = $(window).width();
+        if (width > '992') {
+
+            setTimeout(function() {
+                $('.top-menu').removeClass('top-menu-transition');
+            }, 300);
+        }
+
+
+        if (width < '993') {
+
+            setTimeout(function() {
+                $('.top-menu').addClass('top-menu-transition');
+            }, 300);
+        }
+    });
+
+});
