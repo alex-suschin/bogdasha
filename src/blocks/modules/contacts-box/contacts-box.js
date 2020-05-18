bf10fceac902af7f121373ca3f6ed5112439fca7
alex-suschin <alex_suschin@mail.ru>
2020-05-18 17:34:42 +0300

    contacts-page-desktop

---
+++ src/blocks/modules/contacts-box/contacts-box.js
@@ -0,0 +1,48 @@
+$(function() {
+
+    $('.btn-popup').click(function(e) {
+        $(this).addClass('active');
+
+        var popupCont = $('#popup-contacts');
+
+
+        if (popupCont.hasClass('popup-active') == false) {
+            $('#popup-wrap-contacts').addClass('popup-active');
+            $('#popup-contacts').addClass('popup-active');
+            $('html').addClass('ov-h');
+
+
+            var firstClick = true;
+            $(document).bind('click.myEventPopup', function(e) {
+                if (!firstClick && $(e.target).closest('#popup-contacts').length == 0) {
+                    $('#popup-wrap-contacts').removeClass('popup-active');
+                    $('#popup-contacts').removeClass('popup-active');
+                    $('.btn-popup-buy').removeClass('active');
+                    $(document).unbind('click.myEventPopup');
+                    setTimeout(() => {
+                        $('html').removeClass('ov-h');
+                    }, 300);
+                }
+
+                firstClick = false;
+            });
+        }
+
+        e.preventDefault();
+    });
+
+    $('input[type=tel]').bind("change keyup input click", function() {
+        if (this.value.match(/[^\+0-9]/g)) {
+            this.value = this.value.replace(/[^\+0-9]/g, '');
+        }
+    });
+
+    $('input[type=tel]').on('focus', function() {
+        if ($(this).val() == '') $(this).val('+7');
+    });
+
+    $('input[type=tel]').on('blur', function() {
+        if ($(this).val() == '+7') $(this).val('');
+    });
+
+});
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,3 +1,4 @@
 import "%modules%/header/header";
 import "%modules%/footer/footer"
 import "%modules%/about-slider/about-slider"
+import "%modules%/contacts-box/contacts-box"
